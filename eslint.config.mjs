4ea6bb09f31d44d587d027c5ea53c71103241632
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2026-08-07 16:09:33 +1000

    chore: lint react files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig({
-  files: ["**/*.{js,ts}"],
+  files: ["**/*.{js,ts,jsx,tsx}"],
   extends: [
     js.configs.recommended,
     tseslint.configs.strictTypeChecked,
